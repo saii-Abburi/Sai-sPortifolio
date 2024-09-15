6bc3cf26eeca81f9f6e2046559411e7a271a18b1
Saiinaruto <saipraveenabburi7@gmail.com>
2024-09-15 09:25:43 +0530

    1

--- app.js
+++ app.js
@@ -23,3 +23,36 @@ bgcToggle.onclick = function(){
         }
     }
 }
+
+// let skillOpenCloseToggle = document.getElementsByClassName("skill-header");
+// let skillGrid = document.getElementsByClassName("skill-list-grid");
+// let arrow = document.getElementsByClassName("fa-angle-down");
+
+// skillOpenCloseToggle.addEventListener("click",()=>{
+//     if(skillGrid.style.display.includes('none')){
+//         console.log(200);
+//         skillGrid.style.display = 'block';
+//         setTimeout(() => {
+//             arrow.classList.remove("fa-angle-down");
+//             arrow.classList.add("fa-angle-up");
+//         }, 3000);
+//     }
+//     else{
+//         skillGrid.style.display = 'none';
+//     }
+// })
+
+let skillOpenCloseToggle = document.getElementsByClassName("skill-header");
+let skillGrid = document.getElementsByClassName("skill-list-grid");
+let arrow = document.getElementsByClassName("fa-angle-down");
+
+for (let i = 0; i < skillOpenCloseToggle.length; i++) {
+    skillOpenCloseToggle[i].addEventListener("click", () => {
+        if (skillGrid[i].style.display === 'none') {
+            skillGrid[i].style.display = 'block';
+        } else {
+
+            skillGrid[i].style.display = 'none';
+        }
+    });
+}
